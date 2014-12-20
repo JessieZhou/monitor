045b96407e993446aef389ce852cf356ffa2afe0
chenlingpeng <forrest0579@gmail.com>
2014-12-20 22:24:20 +0800

    refactor

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,7 +1,6 @@
 package controllers
 
 import models._
-import org.apache.commons.codec.digest.DigestUtils
 import org.slf4j.LoggerFactory
 import play.api._
 import play.api.data.Form
@@ -12,49 +11,22 @@ import util._
 import scala.collection.mutable
 
 object Application extends Controller with Secured {
-  val loginForm = Form(tuple("email" -> email, "password" -> nonEmptyText()))
-
-  val registerForm = Form(tuple("username" -> nonEmptyText(), "email" -> email, "password" -> nonEmptyText(), "repassword" -> nonEmptyText(), "role" -> number(), "group" -> longNumber(), "team" -> longNumber()))
 
   // dashboard
-  def index =
-    withAuth { username => implicit request =>
+  def index = withAuth { username => implicit request =>
       val user = User.getUserByEmailFromCache(username)
       val keywords = UserKeyword.getByUid(user.id)
       Ok(views.html.index(User.getUserByEmail(username).get))
     }
 
-  // login page
-  def login = Action { implicit request =>
-    Ok(views.html.neonlogin(loginForm)).withNewSession
-  }
 
-  def logout = Action { implicit request =>
-    Ok(views.html.neonlogin(loginForm)).withNewSession
-  }
 
   def msg(msg: String) = Action { implicit request =>
     val user = User.getUserById(request.session.get("session.id").get.toLong)
     Ok(views.html.index(user))
   }
 
-  def authenticate = Action { implicit request =>
-    loginForm.bindFromRequest.fold(formWithErrors => BadRequest(views.html.neonlogin(formWithErrors)), userInfo => {
-      val user = User.getUserByEmail(userInfo._1)
-      if (user.isDefined) {
-        val md5Hex = DigestUtils.md5Hex(userInfo._2 + user.get.email)
-        if (user.get.password == md5Hex) {
-          Logger.info(s"${user.get.email} login OK")
-          Redirect(routes.Application.index()).withSession("session.id" -> user.get.id.toString, "session.password" -> user.get.password)
-        } else {
-          Logger.info(s"${user.get.email} login fail")
-          Ok(views.html.neonlogin(loginForm.fill((userInfo._1, "")).withError("perror", "password error")))
-        }
-      } else {
-        Ok(views.html.neonlogin(loginForm.fill((userInfo._1, "")).withError("merror", "email invalid")))
-      }
-    })
-  }
+
 
   def javascriptRoutes = Action { implicit request =>
     Ok(Routes.javascriptRouter("jsRoutes")()).as("text/javascript")
@@ -65,7 +37,7 @@ trait Secured {
 
   def username(request: RequestHeader) = request.session.get(Security.username)
 
-  def onUnauthorized(request: RequestHeader) = Results.Redirect(routes.Application.login)
+  def onUnauthorized(request: RequestHeader) = Results.Redirect(routes.UserApp.login)
 
   def withAuth(f: => String => Request[AnyContent] => Result) = {
     Security.Authenticated(username, onUnauthorized) { user =>
--- app/controllers/UserApp.scala
+++ app/controllers/UserApp.scala
@@ -2,9 +2,10 @@ package controllers
 
 import java.io.File
 
-import controllers.Application._
 import models.User
 import org.apache.commons.codec.digest.DigestUtils
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.{Play, Logger}
 import play.api.cache.Cache
 import play.api.libs.json.Json
@@ -18,6 +19,36 @@ import scala.concurrent.duration._
  * Created by chenlingpeng on 2014/11/4.
  */
 object UserApp extends Controller with Secured{
+  val loginForm = Form(tuple("email" -> email, "password" -> nonEmptyText()))
+
+  val registerForm = Form(tuple("username" -> nonEmptyText(), "email" -> email, "password" -> nonEmptyText(), "repassword" -> nonEmptyText(), "role" -> number(), "group" -> longNumber(), "team" -> longNumber()))
+
+  // login page
+  def login = Action { implicit request =>
+    Ok(views.html.neonlogin(loginForm)).withNewSession
+  }
+
+  def logout = Action { implicit request =>
+    Ok(views.html.neonlogin(loginForm)).withNewSession
+  }
+
+  def authenticate = Action { implicit request =>
+    loginForm.bindFromRequest.fold(formWithErrors => BadRequest(views.html.neonlogin(formWithErrors)), userInfo => {
+      val user = User.getUserByEmail(userInfo._1)
+      if (user.isDefined) {
+        val md5Hex = DigestUtils.md5Hex(userInfo._2 + user.get.email)
+        if (user.get.password == md5Hex) {
+          Logger.info(s"${user.get.email} login OK")
+          Redirect(routes.Application.index()).withSession("session.id" -> user.get.id.toString, "session.password" -> user.get.password)
+        } else {
+          Logger.info(s"${user.get.email} login fail")
+          Ok(views.html.neonlogin(loginForm.fill((userInfo._1, "")).withError("perror", "password error")))
+        }
+      } else {
+        Ok(views.html.neonlogin(loginForm.fill((userInfo._1, "")).withError("merror", "email invalid")))
+      }
+    })
+  }
 
   def forgetPsw = Action {
     implicit request =>
